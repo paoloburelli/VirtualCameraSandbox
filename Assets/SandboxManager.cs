d864ba937b663d2e20cdbdb71c5b97bdb87d0855
Paolo Burelli <burelli@gmail.com>
2014-11-26 15:13:10 +0100

    Added the rocks area

--- Assets/SandboxManager.cs
+++ Assets/SandboxManager.cs
@@ -20,33 +20,33 @@ public class SandboxManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		Transform t = GetComponent<Spawn>().SpawnAtSpawnPoint (GameObject.Find ("Forest").transform,0,-Vector2.right);
-		t.GetComponent<RootMotionCharacterController>().Talk();
-
-		shot = new Shot();
-		shot.NumberOfSubjects = 1;
-		shot.Properties.Add (new ProjectionSize(0,1,1));
-		shot.Properties.Add(new VantageAngle(0,0,0,1));
-		shot.SubjectCenters.Add (new Vector3(0,1.6f,0));
-		shot.SubjectScales.Add (new Vector3(.2f,.2f,.2f));
-		shot.SubjectBounds.Add (PrimitiveType.Capsule);
-
-		subjects  = new Subject[shot.NumberOfSubjects];
-		for (int i = 0; i<shot.NumberOfSubjects; i++)
-			subjects[i] = new Subject(t,shot.SubjectCenters[i],shot.SubjectScales[i],shot.SubjectBounds[i]); 
-
-		Camera.main.transform.position = t.position + t.forward;
-
-		Camera.main.transform.LookAt(t.position + Vector3.up*1.6f);
-		Camera.main.transform.Translate(Vector3.up);
-
-		shot.UpdateSubjects(subjects,Camera.main);
-		shot.Evaluate ();
-
-		foreach (Property p in shot.Properties) {
-			Debug.Log(p.Type + ": " + p.Satisfaction);
-		}
-		Debug.Log ("Visibility: " + shot.Visibility);
+//		Transform t = GetComponent<Spawn>().SpawnAtSpawnPoint (GameObject.Find ("Rocks").transform,0,-Vector2.right);
+//		t.GetComponent<RootMotionCharacterController>().Talk();
+//
+//		shot = new Shot();
+//		shot.NumberOfSubjects = 1;
+//		shot.Properties.Add (new ProjectionSize(0,1,1));
+//		shot.Properties.Add(new VantageAngle(0,0,0,1));
+//		shot.SubjectCenters.Add (new Vector3(0,1.6f,0));
+//		shot.SubjectScales.Add (new Vector3(.2f,.2f,.2f));
+//		shot.SubjectBounds.Add (PrimitiveType.Capsule);
+//
+//		subjects  = new Subject[shot.NumberOfSubjects];
+//		for (int i = 0; i<shot.NumberOfSubjects; i++)
+//			subjects[i] = new Subject(t,shot.SubjectCenters[i],shot.SubjectScales[i],shot.SubjectBounds[i]); 
+//
+//		Camera.main.transform.position = t.position + t.forward;
+//
+//		Camera.main.transform.LookAt(t.position + Vector3.up*1.6f);
+//		Camera.main.transform.Translate(Vector3.up);
+//
+//		shot.UpdateSubjects(subjects,Camera.main);
+//		shot.Evaluate ();
+//
+//		foreach (Property p in shot.Properties) {
+//			Debug.Log(p.Type + ": " + p.Satisfaction);
+//		}
+//		Debug.Log ("Visibility: " + shot.Visibility);
 	}
 
 	
